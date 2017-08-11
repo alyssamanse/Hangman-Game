05f50e991e0a7f42c5583256eb0f5c50b84009de
Alyssa Manse <alyssa.manse@austin.utexas.edu>
2017-08-10 23:34:36 -0500

    flow for one round of hangman

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 
 
-var words = ["Stark", "Tyrion", "Lannister", "Drogon", "Westeros", "Varys", "Mountain", "Hound", "Daenerys", "Targaryen", "Khaleesi", "Melisandre", "Valyrian", "Gendry", "Volantis", "Hodor", "Missandei", "Dorne", "Oberyn", "Direwolf", "Podrick", "Raven", "Viserys", "Pentos", "Baratheon", "Dothraki", "Needle", "Wildling", "Brienne", "Ghost", "Shaggydog", "Nymeria"];
+var words = ["stark", "tyrion", "lannister", "drogon", "westeros", "varys", "mountain", "hound", "daenerys", "targaryen", "khaleesi", "melisandre", "valyrian", "gendry", "volantis", "hodor", "missandei", "dorne", "oberyn", "direwolf", "podrick", "raven", "viserys", "pentos", "baratheon", "dothraki", "needle", "Wildling", "brienne", "ghost", "shaggydog", "nymeria"];
 
 var wins = document.getElementById("wins");
 var currentWord = document.getElementById("currentWord");
@@ -13,40 +13,41 @@ var loserImage = document.getElementById("loserImage");
 
 var wins = 0;
 var guessedLetters = [];
-var lives = ["plank", "crossbeam", "noose", "head", "body", "right arm", "left arm", "right leg", "left leg"];
 
 // Randomly chooses a choice from the words array. This is the Computer's word.
 var computerWord = words[Math.floor(Math.random() * words.length)];
 
+// Starting lives in Hangman
+var remainingLives = 9;
+
+// Splits selected word into individuals letters
+var wordLetters = computerWord.split("");
+
+// Calculates the length of the current word
+var wordLength = wordLetters.length;
+ 
+// Creates an array with dashes the same length as the current word
+var blankWord = Array(wordLength).fill("_");
+
 document.onkeyup = function(event) {
 
-     // Splits selected word into individuals letters
-     var wordLetters = computerWord.split("");
-
-     // Calculates the length of the current word
-     var wordLength = wordLetters.length;
-     
-     // Creates an array with dashes the same length as the current word
-     var blankWord = Array(wordLength).fill("_");
-
-     // Determines which key was pressed.
+	// Determines which key was pressed.
      var userGuess = event.key;
 
      /*/ Creates an array from the blank word with the commas removed
      var spaces = [blankWord.join(" ")];
      */
 
-     remainingLives = lives.length;
+     // WRITE SOMETHING TO CAPTURE DOUBLE LETTERS LIKE SS IN MISSANDEI, CURRENTLY ONLY REPLACING FIRST ONEi
+     // REMOVE DUPLICATE LETTERS FROM GUESSEDLETTERS
 
-     currentWord.innerHTML = "<h1> " + blankWord + " </h1";
-     guessesRemaining.innerHTML = "<h3> " + remainingLives + " </h3>";
-     lettersGuessed.innerHTML = "<h2> " + guessedLetters + " </h2>";	
+     console.log(computerWord);
 
-     while (remainingLives > 0) {
+     if (remainingLives >= 1) {
 
 	   	 if (wordLetters.indexOf(userGuess) > -1) {
-	     	blankWord.shift();
-	     	blankWord.unshift(userGuess)
+	     	var choiceIndex = wordLetters.indexOf(userGuess);
+	     	blankWord[choiceIndex] = userGuess;
 	     	guessedLetters.push(userGuess);
 	     } else if (guessedLetters.indexOf(userGuess) > -1) {
 	     	return;
@@ -54,7 +55,9 @@ document.onkeyup = function(event) {
 	     	guessedLetters.push(userGuess);
 	     	remainingLives = (remainingLives - 1);
     	 };
-    };
+    } else {
+    	alert("You lose!")
+    	};
      
      
   
